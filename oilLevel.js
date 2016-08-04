f7d542cd7efb37d43d6807047d4f0e657351e1c2
Chris McKay <mckaycr@gmail.com>
2016-08-04 20:21:11 +0100

    added test script

--- oilLevel.js
+++ oilLevel.js
@@ -1,13 +1,15 @@
 var PythonShell = require('python-shell');
 
 function oilLevel(){
-	this.check = function(callback){
+	this.check = function(callback, options){
 		measure(function(err,res){
 			if(err){callback(err)}
 			else{callback(null,res)}
 		})
 	}
 }
+
+
 function measure(callback){
 	PythonShell.run('measure.py', function (err,results) {
 		    if (err){callback(err)}
