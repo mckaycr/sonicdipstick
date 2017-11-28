1c41fcd2c3fa3c10a15ac96f89b84c700e9f2644
mckaycr <mckaycr@gmail.com>
2017-11-28 13:27:35 -0500

    removed hard coded measurements I was using for testing.  Need to display error if error exists

--- routes/api.js
+++ routes/api.js
@@ -3,19 +3,18 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	res.send({ date: '11/28/2017',data: 20.56,time: '14:00:14' });
- //  	var options = {
-	//     pin:11
-	// }
-	// oil.check(options, function(err,results){
-	//     if(!err){
-	//     	console.log(results)
-	//     	res.send(results);
-	//    	}else{
-	//    		console.log(err)
-	//    		res.send(err)
-	//    	}
-	// })
+  	var options = {
+	    pin:11
+	}
+	oil.check(options, function(err,results){
+	    if(!err){
+	    	console.log(results)
+	    	res.send(results);
+	   	}else{
+	   		console.log(err)
+	   		res.send(err)
+	   	}
+	})
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var results = { date: '11/28/2017',data: 20.56,time: '14:00:14' }
- // 	var options = {
-	//     pin:11
-	// }
-	// oil.check(options, function(err,results){
-	//     if(!err){
-	//     	console.log(results)
-	//     	res.send(results);
-	//    	}else{
-	//    		console.log(err)
-	//    		res.send(err)
-	//    	}
-	// })
-	res.render('pages/index', { title: 'Sonic Dip Stick', data:results.data });
+ 	var options = {
+	    pin:11
+	}
+	oil.check(options, function(err,results){
+	    if(!err){
+	    	console.log(results)
+	    	res.render('pages/index', { title: 'Sonic Dip Stick', data:results.data, error:null });
+	   	}else{
+	   		console.log(err)
+	   		res.render('pages/index', { title: 'Sonic Dip Stick', data:0, error:err });
+	   	}
+	})
 });
 
 module.exports = router;
