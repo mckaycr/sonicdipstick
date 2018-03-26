08d11fe723d69f555f17465b86d4bd0ef1f07c7c
Christopher McKay <webmaster@vfwpost8672.org>
2018-03-26 12:52:55 -0400

    fix indents

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,21 @@ var router = express.Router();
 if(process.env.NODE_ENV != 'development'){
 	var oil = require('../model/oilLevel.js');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
- 	var options = {
-	    pin:11
-	}
-	oil.check(options, function(err,results){
-	    if(!err){
-	    	console.log(results)
-	    	res.render('pages/index', { title: 'Sonic Dip Stick', data:results.data, error:null });
-	   	}else{
-	   		console.log(err)
-	   		res.render('pages/index', { title: 'Sonic Dip Stick', data:0, error:err });
-	   	}
-	})
-});
+	/* GET home page. */
+	router.get('/', function(req, res, next) {
+	 	var options = {
+		    pin:11
+		}
+		oil.check(options, function(err,results){
+		    if(!err){
+		    	console.log(results)
+		    	res.render('pages/index', { title: 'Sonic Dip Stick', data:results.data, error:null });
+		   	}else{
+		   		console.log(err)
+		   		res.render('pages/index', { title: 'Sonic Dip Stick', data:0, error:err });
+		   	}
+		})
+	});
 
 }else{
 	router.get('/', function(req, res, next) {
