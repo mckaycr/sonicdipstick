88cd582e45a2c69c84ae06fa21f33ba0a926b956
mckaycr <mckaycr@gmail.com>
2018-03-26 17:50:08 -0400

    finished collecting basic settings

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,55 @@
 var express = require('express');
 var router = express.Router();
-if(process.env.NODE_ENV != 'development'){
-	var oil = require('../model/oilLevel.js');
+var fs = require('fs');
 
-	/* GET home page. */
-	router.get('/', function(req, res, next) {
-	 	var options = {
-		    pin:11
-		}
-		oil.check(options, function(err,results){
-		    if(!err){
-		    	console.log(results)
-		    	res.render('pages/index', { title: 'Sonic Dip Stick', data:results.data, error:null });
-		   	}else{
-		   		console.log(err)
-		   		res.render('pages/index', { title: 'Sonic Dip Stick', data:0, error:err });
-		   	}
-		})
-	});
 
-}else{
-	router.get('/', function(req, res, next) {
-		var results = {date:'11/29/2017',data: 13.768741607666016,time: '13:38:18' }
-		res.render('pages/index', { title: 'Sonic Dip Stick', data:results.data, error:null });
+		if(process.env.NODE_ENV != 'development'){
+		var oil = require('../model/oilLevel.js');
+
+		/* GET home page. */
+		router.get('/', function(req, res, next) {
+			var options = {
+					pin:11
+			}
+			fs.readFile('model/settings.json', function(err,data){
+    		var settings=JSON.parse(data);
+			oil.check(options, function(err,results){
+					if(!err){
+						console.log(results)
+						res.render('pages/index', {
+							title: settings.device_name, 
+							data:results.data, 
+							error:null,
+							tank_cap:settings.tank_cap,
+							tank_height:settings.tank_height
+						});
+					}else{
+						console.log(err)
+						res.render('pages/index', {
+							title: settings.device_name,
+							data:0,
+							error:err,
+							tank_cap:settings.tank_cap,
+							tank_height:settings.tank_height
+						});
+					}
+			})
+			})
+		});
+
+	}else{
+		router.get('/', function(req, res, next) {
+			fs.readFile('model/settings.json', function(err,data){
+    		var settings=JSON.parse(data);
+				var results = {date:'11/29/2017',data: 13.768741607666016,time: '13:38:18' }
+				res.render('pages/index', {
+					title: settings.device_name,
+					data:results.data,
+					error:null,
+					tank_cap:settings.tank_cap,
+					tank_height:settings.tank_height 
+				});
+		});
 	});
 }
-
-
 module.exports = router;
--- routes/settings.js
+++ routes/settings.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
-
+var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/settings', {title:"Sonic Dip Stick - Settings"});
+ fs.readFile('model/settings.json', function(err,data){
+    var obj = JSON.parse(data);
+    res.render('pages/settings', {
+      title:"Sonic Dip Stick - Settings",
+      device_name:obj.device_name,
+      tank_cap:obj.tank_cap,
+      tank_height:obj.tank_height
+    });
+ })
 });
+router.post('/',function(req, res, next){
+  var settings = JSON.stringify({
+    device_name:req.body.device_name,
+    tank_cap:req.body.tank_capcity,
+    tank_height:req.body.tank_height
+  })
+  fs.writeFile('model/settings.json',settings, 'utf8', function(){
+    res.redirect('/');
+  })
+})
 
 module.exports = router;
