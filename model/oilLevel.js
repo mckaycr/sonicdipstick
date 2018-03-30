731658c05a2f2861a587553b99a26c4190b9af38
mckaycr <mckaycr@gmail.com>
2018-03-30 18:03:21 -0400

    not sure what I changed

--- model/oilLevel.js
+++ model/oilLevel.js
@@ -81,6 +81,6 @@ function calc(m, tk_ht,tk_cap){
       percentile = ((residual/tk_ht)*100).toFixed(1);
 		  gals = ((tk_cap*percentile)/100).toFixed(1);
   }
-  return {'residual':residual, 'percentile':percentile, 'gallons':gals}
+	return {'residual':residual, 'percentile':percentile, 'gallons':gals}
 }
 
