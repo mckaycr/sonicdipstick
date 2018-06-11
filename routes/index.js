dd0f545e26c42456f18fcbbf40b872ea6e736000
Christopher McKay <webmaster@vfwpost8672.org>
2018-06-11 11:04:28 -0400

    adding mongo query to dev mode

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,6 @@ var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://192.168.0.151:27017/";
-var settings = {}
-// MongoClient.connect(url, function(err, db) {
-//   if (err) throw err;
-//   var dbo = db.db("sdsdata");
-//   dbo.collection("settings").findOne({}, function(err, result) {
-//     if (err) throw err;
-//     settings=result
-//     console.log(settings)
-//     db.close();
-//   });
-// });
 	if(process.env.NODE_ENV != 'development'){
 	var oil = require('../model/oilLevel.js');
 	/* GET home page. */
@@ -58,6 +47,17 @@ var settings = {}
 	}else{
 // 		***************** Develompent Mode ********************
 		router.get('/', function(req, res, next) {
+			var settings = {}
+			MongoClient.connect(url, function(err, db) {
+			  if (err) throw err;
+			  var dbo = db.db("sdsdata");
+			  dbo.collection("settings").findOne({}, function(err, result) {
+			    if (err) throw err;
+			    settings=result
+			    console.log(settings)
+			    db.close();
+			  });
+			});
 			var results = {date:'11/29/2017',data:{'residual':'26', 'percentile':59,'gallons':162},time: '13:38:18' }
 			res.render('pages/index', {
 				title: settings.device_name,
