83613cf28292e995a485e5c65106b61f929bfcdd
Christopher McKay <webmaster@vfwpost8672.org>
2018-06-11 12:20:06 -0400

    moving global variables out where they belong

--- routes/settings.js
+++ routes/settings.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://mongdb:27017/";
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var MongoClient = require('mongodb').MongoClient;
-	var url = "mongodb://mongdb:27017/";
 	  MongoClient.connect(url, function(err, db) {
 	    if (err) throw err;
 	    var dbo = db.db("sdsdata");
