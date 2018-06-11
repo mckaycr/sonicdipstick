68fc42da9f754a926b9353a7ac84f60da8f38e88
Christopher McKay <webmaster@vfwpost8672.org>
2018-06-11 09:59:27 -0400

    removing obsolete lines of code and cleaning up indents

--- routes/index.js
+++ routes/index.js
@@ -13,59 +13,49 @@ MongoClient.connect(url, function(err, db) {
     db.close();
   });
 });
-
-//var settingsPath = '/data/settings.json'
-		
-		if(process.env.NODE_ENV != 'development'){
-		var oil = require('../model/oilLevel.js');
-		/* GET home page. */
-		router.get('/', function(req, res, next) {
-			var options = {
-					pin:11
-			}
-			//fs.readFile(settingsPath, function(err,data){
-    		//var settings=JSON.parse(data);
-			oil.check(options, function(err,results){
-					if(!err){
-						console.log(results)
-						res.render('pages/index', {
-							title: settings.device_name, 
-							data:results.data, 
-							error:null,
-							tank_cap:settings.tank_cap,
-							tank_height:settings.tank_height,
-							unit_display:settings.unit_display
-						});
-					}else{
-						console.log(err)
-						res.render('pages/index', {
-							title: settings.device_name,
-							data:0,
-							error:err,
-							tank_cap:settings.tank_cap,
-							tank_height:settings.tank_height,
-							unit_display:settings.unit_display
-						});
-					}
-			})
-			//})
-		});
-
-	}else{
-// 		***************** Develompent Mode ********************
-		router.get('/', function(req, res, next) {
-			//fs.readFile(settingsPath, function(err,data){
-    		//var settings=JSON.parse(data);
-				var results = {date:'11/29/2017',data:{'residual':'26', 'percentile':59,'gallons':162},time: '13:38:18' }
+	if(process.env.NODE_ENV != 'development'){
+	var oil = require('../model/oilLevel.js');
+	/* GET home page. */
+	router.get('/', function(req, res, next) {
+		var options = {
+				pin:11
+		}
+		oil.check(options, function(err,results){
+			if(!err){
+				console.log(results)
 				res.render('pages/index', {
-					title: settings.device_name,
-					data:results.data,
+					title: settings.device_name, 
+					data:results.data, 
 					error:null,
 					tank_cap:settings.tank_cap,
 					tank_height:settings.tank_height,
 					unit_display:settings.unit_display
 				});
-		//});
+			}else{
+				console.log(err)
+				res.render('pages/index', {
+					title: settings.device_name,
+					data:0,
+					error:err,
+					tank_cap:settings.tank_cap,
+					tank_height:settings.tank_height,
+					unit_display:settings.unit_display
+				});
+			}
+		})
+	});
+	}else{
+// 		***************** Develompent Mode ********************
+		router.get('/', function(req, res, next) {
+			var results = {date:'11/29/2017',data:{'residual':'26', 'percentile':59,'gallons':162},time: '13:38:18' }
+			res.render('pages/index', {
+				title: settings.device_name,
+				data:results.data,
+				error:null,
+				tank_cap:settings.tank_cap,
+				tank_height:settings.tank_height,
+				unit_display:settings.unit_display
+			});
 	});
 }
 module.exports = router;
